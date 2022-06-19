34b85cee38ed66333e500f26d906dab09193ebbf
Kade Boltjes <54650222+Kboltjes@users.noreply.github.com>
2022-06-19 14:04:05 -0400

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+/*
+	Fractal by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Mobile?
+		if (browser.mobile)
+			$body.addClass('is-mobile');
+		else {
+
+			breakpoints.on('>medium', function() {
+				$body.removeClass('is-mobile');
+			});
+
+			breakpoints.on('<=medium', function() {
+				$body.addClass('is-mobile');
+			});
+
+		}
+
+	// Scrolly.
+		$('.scrolly')
+			.scrolly({
+				speed: 1500
+			});
+
+})(jQuery);
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+        this.innerHTML = "<p style=\"color: white;\">Pictures/Videos<i style=\"float: right; margin-top: 17px;\" class=\"fas fa-plus\"></i></p>";
+    } else {
+        content.style.display = "block";
+        this.innerHTML = "<p style=\"color: white;\">Pictures/Videos<i style=\"float: right; margin-top: 17px;\" class=\"fas fa-minus\"></i></p>";
+    }
+  });
+}
